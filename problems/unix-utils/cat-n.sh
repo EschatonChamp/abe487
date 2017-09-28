1688391145996f954c936792a9bdfc7a38f3b667
EschatonChamp <jacobhcecil@gmail.com>
2017-09-27 21:15:48 -0700

    all good

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "OK"
+if [[ $# -ne 1 ]]; then
+    printf "Usage: %s FILE\n" "$(basename "$0")"
+    exit 1
+fi
+
+FILE=$1
+if [[ -f $FILE ]]; then
+x=0
+while read -r LINE; do
+    let x++
+    echo "$x $LINE"
+done < $FILE
+else 
+	echo "\"$1\" is not a file"
+	exit 1
+fi
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+if [[ $# -lt 1 ]]; then
+	printf "Usage: %s FILE [NUM]\n" "$(basename "$0")"
+fi
+
+FILE=$1
+NUM=${2:-3}
+if [[ -f $FILE ]]; then
+x=0
+while read -r LINE; do
+let x++
+		if [[ x -lt $NUM ]] || [[ x -eq $NUM ]]; then
+		echo "$LINE"
+		fi
+done < "$FILE"
+else
+	echo "\"$1\" is not a file"
+	exit 1
+fi
+
+
