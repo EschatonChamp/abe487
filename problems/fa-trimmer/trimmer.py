3b1b02ed31ec5dc5a2dfa317f93343c78123973e
EschatonChamp <jacobhcecil@gmail.com>
2017-11-02 13:33:05 -0700

    thanks ken

---
+++ problems/fa-trimmer/trimmer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+from Bio import SeqIO
+# --------------------------------------------------
+def get_args():
+	parser = argparse.ArgumentParser(description='FASTA trimmer')
+	parser.add_argument('file', help='FASTA file', metavar='fasta_file')
+	parser.add_argument('-s', '--start', help='Start position', metavar='int', type=int, default=0)
+	parser.add_argument('-e', '--end', help='A named integer argument', metavar='int', type=int, default=None)
+	parser.add_argument('-m', '--min', help='A boolean flag', metavar='int', type=int, default=0)
+	parser.add_argument('-o', '--outfile', help='Output file', metavar='str', type=str, default='')
+	return parser.parse_args()
+
+# --------------------------------------------------
+def main():
+	args = get_args()
+	file = args.file
+	start_pos  = args.start
+	end_pos = args.end
+	min_len = args.min
+	outfile = args.outfile
+	
+	if len(outfile) == 0:
+		outfile = file + '.trimmed'
+
+	out_fh = open(outfile, 'w')
+
+	num_taken = 0
+	if start_pos > 0:
+		start_pos -= 1
+	with open(file) as handle:
+		for record in SeqIO.parse(handle, "fasta"):
+			seq = str(record.seq[start_pos:end_pos])
+			if len(seq) >= min_len:
+				num_taken += 1
+				out_fh.write('\n'.join(['>' + record.id, seq, '']))
+			
+
+	print('Wrote {} sequence{} to "{}"'.format(num_taken, '' if num_taken == 1 else 's', outfile))
+
+
+
+#-------------------------------------------
+if __name__ == '__main__':
+    main()
